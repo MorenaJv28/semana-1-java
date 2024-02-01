41bbe5eea2d59ff1a0bbf472e58e10ebfd0aa534
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2024-01-31 18:43:56 -0600

    ejercicio 2

--- Ejercicio2-Morena Gutierrez/Main/src/main/java/com/mycompany/main/Main.java
+++ Ejercicio2-Morena Gutierrez/Main/src/main/java/com/mycompany/main/Main.java
@@ -3,11 +3,15 @@ package com.mycompany.main;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
-        String PrimerNumero, SegundoNumero;
-        int Num1, Num2, Suma;
+    public static void main(String[] args)
+    {
+        String PrimerNumero;
+        String SegundoNumero;
+        int Num1;
+        int Num2;
+        int Suma;
 
-        PrimerNumero = JOptionPane.showInputDialog("Escribe el primer entero: ");
+        PrimerNumero =JOptionPane.showInputDialog("Escribe el primer entero: ");
         SegundoNumero = JOptionPane.showInputDialog("Escribe el segundo entero: ");
 
         Num1 = Integer.parseInt(PrimerNumero);
